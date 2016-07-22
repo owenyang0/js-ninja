47bff1a31b31e1692e16bffbf19fd6dade8b5a96
Owen Yang <usay@outlook.com>
2016-07-22 16:29:39 +0800

    part 1 - compress final solution

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,21 +1,11 @@
+'use strict'
 
 function compress (str) {
-  var alphaReg = /^[a-z]+$/
-  if (!alphaReg.test(str)) throw Error('invalid input')
+  if (!/^[a-z]+$/.test(str)) throw Error('invalid input')
 
-  var acc = 1
   return str
-    .split('')
-    .reduce((pre, curr) => {
-      if (curr === pre.slice(pre.length - 1)) {
-        acc++
-      } else {
-        pre += acc + curr
-        acc = 1
-      }
-
-      return pre
-    }) + acc
+    .match(/([a-z])\1*/g)
+    .reduce((pre, curr) => pre + curr[0] + curr.length, '')
 }
 
 module.exports = {
--- javascript/test/test.js
+++ javascript/test/test.js
@@ -3,7 +3,9 @@ const compress = require('../src').compress
 
 describe('compress', () => {
   it('should throw an error for invalid input', () => {
-    () => compress('123').should.throw(/invalid input/)
+    ;(() => compress('123')).should.throw(/invalid input/)
+    ;(() => compress('')).should.throw(/invalid input/)
+    ;(() => compress('123@xxxx3333')).should.throw(/invalid input/)
   })
 
   it('should return a4b2a4b1a1b3c12 for aaaabbaaaababbbcccccccccccc', () => {
