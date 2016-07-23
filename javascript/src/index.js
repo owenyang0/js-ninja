aa86ecf94017eb3d429f7fda0a803ecf08c15e98
Owen Yang <usay@outlook.com>
2016-07-24 00:32:56 +0800

    part 2 - checkURIs initial solution

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const url = require('url')
+const path = require('path')
+
 function compress (str) {
   if (!/^[a-z]+$/.test(str)) throw Error('invalid input')
 
@@ -8,6 +11,34 @@ function compress (str) {
     .reduce((pre, curr) => pre + curr[0] + curr.length, '')
 }
 
+function checkURIs (uri1, uri2) {
+  const uriReg = /^https?:\/\/[^\s\/$.?#].[^\s]*$/i
+  if (!uriReg.test(uri1) || !uriReg.test(uri2)) throw Error('invalid uri')
+
+  let uri1Obj = url.parse(uri1)
+  let uri2Obj = url.parse(uri2)
+
+  const convertedUri = [uri1Obj, uri2Obj].map(uri => {
+    if (uri.port === '80') {
+      uri.port = null
+      uri.host = null
+    }
+
+    uri.pathname = path.normalize(uri.pathname)
+
+    uri.search = uri.query
+      ? uri.query.split('&').sort().join('&')
+      : null
+
+    return decodeURIComponent(url.format(uri))
+  })
+
+  const isSame = convertedUri.every((curr, idx, arr) => curr === arr[0])
+
+  return isSame
+}
+
 module.exports = {
-  compress: compress
+  compress: compress,
+  checkURIs: checkURIs
 }
--- javascript/test/test.js
+++ javascript/test/test.js
@@ -1,5 +1,6 @@
 const should = require('should')
 const compress = require('../src').compress
+const checkURIs = require('../src').checkURIs
 
 describe('compress', () => {
   it('should throw an error for invalid input', () => {
@@ -10,5 +11,97 @@ describe('compress', () => {
 
   it('should return a4b2a4b1a1b3c12 for aaaabbaaaababbbcccccccccccc', () => {
     compress('aaaabbaaaababbbcccccccccccc').should.be.equal('a4b2a4b1a1b3c12')
+  })
+})
+
+describe('checkURIs', () => {
+  const baseUri = 'http://abc.com/~smith/home.html'
+
+  it('should pass two valid params', () => {
+    ;(() => checkURIs(baseUri, 'invalid input')).should.throw(/invalid uri/)
+    ;(() => checkURIs('invalid input', baseUri)).should.throw(/invalid uri/)
+  })
+
+  it('should be equivalent for absoulutely same uris', () => {
+    checkURIs(baseUri, baseUri).should.be.a.Boolean();
+    checkURIs(baseUri, baseUri).should.be.true();
+  })
+
+  it('should not be equivalent for different uris', () => {
+    const uri2 = 'http://abc.com/smith/home.html'
+    checkURIs(baseUri, uri2).should.be.false();
+  })
+
+  it('should be equivalent for uri of port 80', () => {
+    const uri2 = 'http://abc.com:80/~smith/home.html'
+    const uri3 = 'http://abc.com:81/~smith/home.html'
+
+    checkURIs(baseUri, uri2).should.be.true();
+    checkURIs(baseUri, uri3).should.be.false();
+  })
+
+  it('should return true for case-insensitive scheme names', () => {
+    const uri2 = 'hTtP://abc.com/~smith/home.html'
+
+    checkURIs(baseUri, uri2).should.be.true();
+  })
+
+  it('should return true for case-insensitive hostname', () => {
+    const uri2 = 'hTtP://aBc.cOm/~smith/home.html'
+
+    checkURIs(baseUri, uri2).should.be.true();
+  })
+
+  it('should return false for case-sensitive path, hash, and querystring', () => {
+    const currUri = 'http://abc.com/foo.html?a=1&b=2#hash'
+    const uriForPath = 'http://abc.com/Foo.html?a=1&b=2#hash'
+    const uriForHash = 'http://abc.com/foo.html?a=1&b=2#hAsh'
+    const uriForQuery = 'http://abc.com/foo.html?A=1&b=2#hash'
+
+    checkURIs(currUri, uriForPath).should.be.false();
+    checkURIs(currUri, uriForHash).should.be.false();
+    checkURIs(currUri, uriForQuery).should.be.false();
+  })
+
+  it('should return true for appropriate traversal tokens', () => {
+    const currUri = 'http://abc.com/drill/down/foo.html'
+    const uriForToken = 'http://abc.com/drill/further/../down/./foo.html'
+
+    checkURIs(currUri, uriForToken).should.be.true();
+  })
+
+  it('should be equivalent for their % HEX HEX encodings', () => {
+    const currUri = 'http://abc.com:80/~smith/home.html'
+    const uriForToken = 'http://ABC.com/%7Esmith/home.html'
+
+    checkURIs(currUri, uriForToken).should.be.true();
+  })
+
+  it('should be equivalent for arbitrary order of querystring', () => {
+    const currUri = 'http://abc.com/foo.html?a=1&b=2'
+    const uriForToken = 'http://abc.com/foo.html?b=2&a=1'
+
+    checkURIs(currUri, uriForToken).should.be.true();
+  })
+
+  it('should be equivalent for arbitrary order of querystring for same key', () => {
+    const currUri = 'http://abc.com/foo.html?a=1&b=2&a=2'
+    const uriForToken = 'http://abc.com/foo.html?b=2&a=1&a=2'
+
+    checkURIs(currUri, uriForToken).should.be.true();
+  })
+
+  it('should not be equivalent for different value of querystring key', () => {
+    const currUri = 'http://abc.com/foo.html?a=1&b=1&a=2'
+    const uriForToken = 'http://abc.com/foo.html?b=2&a=1&a=2'
+
+    checkURIs(currUri, uriForToken).should.be.false();
+  })
+
+  it('should be case-sensitive for auth uri', () => {
+    const currUri = 'http://uname:passwd@host.com/foo/bar.html'
+    const uriForAuth = 'http://uName:passwd@host.com/foo/bar.html'
+
+    checkURIs(currUri, uriForAuth).should.be.false();
   })
 })
